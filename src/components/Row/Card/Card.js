d53a576391dd4b4a3ae081a3da3459053c042ac6
Vanture <itsvanture@gmail.com>
2020-01-08 14:04:37 +0100

    Using the Sportnoord API

--- src/components/Row/Card/Card.js
+++ src/components/Row/Card/Card.js
@@ -1,73 +1,87 @@
-import React, { Fragment } from 'react';
-import {
-    View,
-    Text,
-    Image,
-    Button,
-    TouchableHighlight,
-} from "react-native";
+import React, { Fragment } from "react";
+import { View, Text, Image, Button, TouchableHighlight } from "react-native";
 
 import styles from "./card.scss";
 // import Video from 'react-native-video';
 
 export default class Card extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isSelected: false,
-            id: this.props.id,
-            name: this.props.name,
-            genre: this.props.genre
-        }
+    this.state = {
+      isSelected: false,
+      id: this.props.id,
+      title: this.props.title,
+      type: this.props.type,
+      image: this.props.image,
+      videoId: this.props.videoId,
+      date: this.props.date,
+      videoUrl: this.props.videoUrl
+    };
 
-        this.onPress = this.onPress.bind(this);
-        this.onSelect = this.onSelect.bind(this);
-        this.onExit = this.onExit.bind(this);
-    }
+    this.onPress = this.onPress.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+    this.onExit = this.onExit.bind(this);
+  }
 
-    onPress = () => {
-        console.log('#onPress');
-    }
+  onPress = () => {
+    console.log("#onPress");
+  };
 
-    onSelect = () => {
-        console.log('#onSelect');
-        this.setState({ isSelected: true });
-    }
+  onSelect = () => {
+    console.log("#onSelect");
+    this.setState({ isSelected: true });
+  };
 
-    onExit = () => {
-        console.log('#onExit');
-        this.setState({ isSelected: false });
-    }
+  onExit = () => {
+    console.log("#onExit");
+    this.setState({ isSelected: false });
+  };
 
-    render() {
-        const { isSelected, id, name, genre } = this.state;
+  render() {
+    const { isSelected, id, title, type, image, videoUrl } = this.state;
 
-        const appName = config.displayName.toUpperCase();
+    console.log(videoUrl)
 
-        return (
-            <>
-                <TouchableHighlight
-                    key={id}
-                    onPress={() => this.onPress()}
-                    onShowUnderlay={() => this.onSelect()}
-                    onHideUnderlay={() => this.onExit()}
-                    activeOpacity={0.7}
-                    underlayColor={'transparent'}>
+    const appName = config.displayName.toUpperCase();
 
-                    <View>
-                        <View style={[styles.imageContainer], isSelected ? styles.selectedCard : styles.unselectedCard}>
-                            <Image source={isSelected ? { uri: 'https://via.placeholder.com/350/FFA400/EBEBEBE/?text=' + name } : { uri: 'https://via.placeholder.com/350/C3272B/EBEBEBE/?text=' + name }}
-                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                            />
-                        </View>
-                        <View>
-                            <Text style={styles.cardTitle}>{name}</Text>
-                            <Text style={styles.cardGenre}>{appName} <Text style={styles.icon}>&#9679;</Text> {genre}</Text>
-                        </View>
-                    </View>
-                </TouchableHighlight>
-            </>
-        );
-    }
+    return (
+      <>
+        <TouchableHighlight
+          key={id}
+          onPress={() => this.onPress()}
+          onShowUnderlay={() => this.onSelect()}
+          onHideUnderlay={() => this.onExit()}
+          activeOpacity={0.7}
+          underlayColor={"transparent"}
+        >
+          <View>
+            <View
+              style={
+                ([styles.imageContainer],
+                isSelected ? styles.selectedCard : styles.unselectedCard)
+              }
+            >
+              <Image
+                source={{ uri: image }}
+                style={{
+                  flex: 1,
+                  width: null,
+                  height: null,
+                  resizeMode: "cover"
+                }}
+              />
+            </View>
+            <View>
+              <Text style={styles.cardTitle}>{title}</Text>
+              <Text style={styles.type}>
+                {appName}
+                <Text style={styles.icon}>&#9679;</Text> {type}
+              </Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </>
+    );
+  }
 }
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,76 +1,125 @@
-import React, { Fragment } from 'react';
-import axios from 'axios';
-import {
-    View,
-    ScrollView,
-    Text,
-} from "react-native";
+import React, { Fragment } from "react";
+import axios from "axios";
+import { View, ScrollView, Text } from "react-native";
 
 import Card from "./Card/Card";
 
 import styles from "./row.scss";
 
+/**
+ * @async
+ * @param {string} link the id for the link.
+ * @returns the Id of the video.
+ *
+ * @author Tim Twiest
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+const getId = link => {
+  return link.split("/c/")[1].replace(".js", "");
+};
+
 export default class Row extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isLoading: true,
-            cache: [],
-        };
+    this.state = {
+      isLoading: true,
+      cache: []
+    };
 
-        this.onScroll = this.onScroll.bind(this);
-        this.fetchData = this.fetchData.bind(this);
-    }
+    this.onScroll = this.onScroll.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+  }
 
-    onScroll = () => {
-        console.log('#onScroll');
-    }
+  onScroll = () => {
+    console.log("#onScroll");
+  };
 
-    fetchData = () => {
-        axios.get(`https://vanture.io/api/api.json`).then(res => {
-            const cache = res.data;
-            this.setState({
-                cache: cache,
-                isLoading: false
-            });
-        }).catch(error => {
-            if (error.response) {
-                console.log(error.responderEnd);
-            }
+  fetchData = () => {
+    const videoUrl = `https://sportnoord.nl/wp-json/wp/v2/sn-match/ondemand?page=1`;
+    
+    axios
+      .get(videoUrl)
+      .then(res => {
+        const cache = res.data;
+        const data = [];
+
+        cache.forEach(element => {
+          let video = {
+            id: element.id,
+            title: element.title.rendered,
+            type: element.match_sport,
+            image: element.featured_image.medium_large,
+            videoId: getId(element.videos[0].video),
+            date: element.match_dates.start,
+            videoUrl: `https://ndc.bbvms.com/mediaclip/${getId(
+              element.videos[0].video
+            )}/redirect/MP4_HD`
+          };
+
+          data.push(video);
         });
-    }
 
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    render() {
-        const { isLoading, cache } = this.state;
-
-        const Content = () => {
-            return isLoading ? <Text style={styles.errorText}>{'{'}LOADING...{'}'}</Text> : cache.map(child => <Card
-                key={child.id} id={child.id} name={child.name} genre={child.genre} />)
+        this.setState({
+          cache: data,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        if (error.response) {
+          console.log(error.responderEnd);
         }
+      });
+  };
 
-        return (
-            <>
-                <View style={styles.rowContainer}>
+  componentDidMount() {
+    this.fetchData();
+  }
 
-                    <Text style={styles.categoryTitle}>{'{'}CATEGORY{'}'}</Text>
-                    <ScrollView
-                        onScroll={this.onScroll()}
-                        keyboardShouldPersistTaps='always'
-                        keyboardDismissMode='on-drag'
-                        snapToAlignment='start'
-                        contentInsetAdjustmentBehavior="automatic"
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}>
+  render() {
+    const { isLoading, cache } = this.state;
 
-                        < Content />
-                    </ScrollView>
-                </View>
-            </>
-        );
-    }
+    const Content = () => {
+      return isLoading ? (
+        <Text style={styles.errorText}>
+          {"{"}LOADING...{"}"}
+        </Text>
+      ) : (
+        cache.map(child => (
+          <Card
+            key={child.id}
+            id={child.id}
+            title={child.title}
+            type={child.type}
+            image={child.image}
+            videoId={child.videoId}
+            date={child.date}
+            videoUrl={child.videoUrl}
+          />
+        ))
+      );
+    };
+
+    return (
+      <>
+        <View style={styles.rowContainer}>
+          <Text style={styles.categoryTitle}>
+            {"{"}CATEGORY{"}"}
+          </Text>
+          <ScrollView
+            onScroll={this.onScroll()}
+            keyboardShouldPersistTaps="always"
+            keyboardDismissMode="on-drag"
+            snapToAlignment="start"
+            contentInsetAdjustmentBehavior="automatic"
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Content />
+          </ScrollView>
+        </View>
+      </>
+    );
+  }
 }
